01b24b61158347634be7d7e24103695b38fc9d92
theseg148 <52212189+theseg148@users.noreply.github.com>
2026-09-21 20:23:51 +0300

    Launch CornBox V8 social experience

--- CornBox/Sources/CornBoxApp.swift
+++ CornBox/Sources/CornBoxApp.swift
@@ -2,14 +2,10 @@ import SwiftUI
 
 @main
 struct CornBoxApp: App {
-    init() {
-        V6PlaybackRepair.install()
-    }
-
+    init() { V6PlaybackRepair.install() }
     var body: some Scene {
         WindowGroup {
-            RootViewV7()
-                .preferredColorScheme(.dark)
+            RootViewV8().preferredColorScheme(.dark)
         }
     }
 }
